10e79a73d86feb8e3d70dfdbb0b2778e3a84b34d
Nikolas Garofil <nikolas@garofil.be>
2020-03-05 21:15:35 +0100

    Own errors, check for unknown options

--- cmdline/main.cpp
+++ cmdline/main.cpp
@@ -15,15 +15,28 @@ struct settings {
 	bool help = false;
 } settings;
 
+//sends standard errormessage plus 'extrainfo' to STDERR, exits with 'errorcode'
+void showerror(int errorcode, const char* extrainfo) {
+	cerr << "ERROR " << errorcode << ": ";
+	switch(errorcode) {
+	case 1: cerr << "Unknown option '" << extrainfo << "'"; break;
+	default: cerr <<"Unknown error";
+	}
+	cerr << endl;
+	exit(errorcode);
+}
+
 //send help to STDOUT, doesn't exit
 void showhelp() { cout << "TODO" << endl; }
 
 //set settings and show errors for unknown args
 void args_to_settings(int argc, char **argv) {
 	int current_cmdarg;	//Although we use this for regular chars, getopt_long returns int's (also needed for the -1 check)
+	opterr = 0;	//Don't use the system's default errors
 	while ((current_cmdarg = getopt_long(argc, argv, short_options, long_options, NULL)) != -1) {
 		switch(current_cmdarg) {
 		case 'h': settings.help = true; break;
+		default: optind--; showerror(1, argv[optind]);
 		}
 	}
 }
