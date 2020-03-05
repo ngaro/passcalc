031f64d94993966cbfc246052b7c524482d0ed8c
Nikolas Garofil <nikolas@garofil.be>
2020-03-05 20:58:39 +0100

    Begin showhelp(), begin settings

--- cmdline/main.cpp
+++ cmdline/main.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
 
+using namespace std;
+
+//configuration of passcalc
+struct settings {
+	bool help = false;
+} settings;
+
+//send help to STDOUT, doesn't exit
+void showhelp() { cout << "TODO" << endl; }
+
 int main(int argc, char **argv) {
+	if(settings.help) { showhelp(); return EXIT_SUCCESS; }
 	return EXIT_SUCCESS;
 }
